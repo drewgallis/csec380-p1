1cbcf7d4ec4b92698f7b7320adcdd9600bcbd795
drewgallis <drewgallis@gmail.com>
2019-02-06 13:07:25 -0500

    Updating Nginx Config
    fixing nginx's config to work with docker properly

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker container rm -f nginx_web
-docker build -t nginx_web ./activity1/nginx
+docker build -t nginx_web ./nginx
 docker run -d -p 81:80 -l traefik.weight=10 --name nginx_web nginx_web:latest
