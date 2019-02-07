c519cd46207b5ffe673d496401d553ef67653f75
Drew Gallis <drewgallis@gmail.com>
2019-02-07 11:30:24 -0500

    update webtest.py (part2)
    stilling running into travisCI errors, uploading again

--- nginx/webtest.py
+++ nginx/webtest.py
@@ -6,7 +6,7 @@ def webtest():
     req = requests.get('http://localhost:81')
     soup = BeautifulSoup(req.text, 'html.parser')
     print(soup.p.string)
-    assert soup.p.string == 'Hello World - 1'
-    assert 'Hello World - 1' in req.text
+    assert soup.p.string == "Hello World - 1"
+    assert "Hello World - 1" in req.text
 
 webtest()
