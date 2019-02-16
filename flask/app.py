54558d2dc4ac6ee8315b432ce033c40d2d6eb6e3
Drew Gallis <drewgallis@gmail.com>
2019-02-16 16:54:37 -0500

    Adding assets and sample login page (part 2)

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,13 @@
-from flask import Flask
- 
+from flask import Flask,request, render_template, session
+import requests, socket, os 
 app = Flask(__name__)
  
  
 @app.route('/', methods=['GET', 'POST'])
 def login():
     session['logged_in'] = False
-    ip = user_ip()
     host = socket.gethostname() 
-    
+    ip = "test"
     if request.method == 'POST':
         if request.form['user'] == 'admin' and request.form['password'] == '19970902':
             session['logged_in'] = True
@@ -21,4 +20,8 @@ def login():
  
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
+<<<<<<< HEAD
+    app.run(debug=True, host='127.0.0.1', port=5000)
+=======
     app.run(debug=True, host='0.0.0.0')
+>>>>>>> f5ac0f44413ce85d07460adac688645a30596ef3
