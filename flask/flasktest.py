7eaec0142da15fff33aa430931e7690d923ef228
Drew Gallis <drewgallis@gmail.com>
2019-02-16 17:24:32 -0500

    Flask sample login test (part 9)

--- flask/flasktest.py
+++ flask/flasktest.py
@@ -5,6 +5,6 @@ def webtest():
     options = Options() # get firefox webdriver options
     options.add_argument('-headless') # run tests in headless mode CMD
     firefox = Firefox(firefox_options=options) # intialize firefox web driver
-    firefox.get('http://localhost:5000') # test against nginx web
+    firefox.get('http://localhost:5000') # test against flask app
     firefox.close()
 webtest()
