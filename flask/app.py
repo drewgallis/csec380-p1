814fd217cc759fd5a5989f90b75e28bbef9636ce
Drew Gallis <drewgallis@gmail.com>
2019-02-16 16:29:11 -0500

    Adding assets and a sample login page
    Added personal login page and assets to test for a successful login

--- flask/app.py
+++ flask/app.py
@@ -3,9 +3,22 @@ from flask import Flask
 app = Flask(__name__)
  
  
-@app.route('/')
-def hello_whale():
-    return "Welcome, Hello there!"
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    session['logged_in'] = False
+    ip = user_ip()
+    host = socket.gethostname() 
+    
+    if request.method == 'POST':
+        if request.form['user'] == 'admin' and request.form['password'] == '19970902':
+            session['logged_in'] = True
+            return redirect('/status')
+        else:
+            session['logged_in'] = False
+
+    return render_template('auth.html', ip=ip, host=host)
+
  
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(debug=True, host='0.0.0.0')
