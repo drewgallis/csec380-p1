d69ad63edda84cf3941e7b578a838c5d5556f828
Drew Gallis <drewgallis@gmail.com>
2019-04-09 18:02:19 -0400

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -7,7 +7,6 @@ from utils import *
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
