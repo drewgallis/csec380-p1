951a24ea595fcf89749312d8461faa4a3608760d
Drew Gallis <drewgallis@gmail.com>
2019-04-18 19:12:54 -0400

    try selenium test part 8

--- flask/tests/video_test/sample_upload.py
+++ flask/tests/video_test/sample_upload.py
@@ -3,25 +3,9 @@ from selenium.webdriver.firefox.options import Options
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 import time
-def get_hash(password):
-    return generate_password_hash(password)
-
-def check_password(pw_hash, password):
-    return check_password_hash(pw_hash, password)
     
 def getMysqlConnection():
     return mysql.connector.connect(user='root', host='172.17.0.1', port='33060', password='test123', database='mydb')
-
-def initDB():
-    connection = getMysqlConnection()
-    cursor = connection.cursor()
-    sql = "INSERT INTO `User` (`username`, `password`) VALUES (%s, %s)"
-    pw_hash = get_hash('test')
-    cursor.execute(sql, ('test123', pw_hash))
-    connection.commit()
-    print("DB Added User:test123 " + "password:" + pw_hash)
-    cursor.close()
-    connection.close()
 
 def getlogin(firefox):
     firefox.get('http://localhost:5000/login') # test against flask app
@@ -42,7 +26,7 @@ def upload_video(firefox, video_name):
     filename = firefox.find_element_by_name('filename')
     filename.send_keys(str(video_name))
     file_path = firefox.find_element_by_name('file')
-    file_path.send_keys("C://luffytest.jpg")
+    file_path.send_keys("./luffytest.jpg")
     uploadBTN = firefox.find_element_by_id('UploadFile')
     uploadBTN.click()
     if "Successfully Uploaded File:" in firefox.page_source:
