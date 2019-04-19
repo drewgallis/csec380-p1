22338985077aeb9095a1c066195937410f7e1c84
Drew Gallis <drewgallis@gmail.com>
2019-04-18 20:11:38 -0400

    final final selenium upload test

--- flask/tests/video_test/sample_upload.py
+++ flask/tests/video_test/sample_upload.py
@@ -21,32 +21,39 @@ def getlogin(firefox):
         print("Success Caught: Valid User Login!")
     return firefox
 
-def upload_video(firefox, video_name):
+def upload_video(firefox, video_name, file_name):
     firefox.get('http://localhost:5000/') # test against flask app
     time.sleep(5)
     filename = firefox.find_element_by_name('filename')
     filename.send_keys(str(video_name))
     file_path = firefox.find_element_by_name('file')
-    file_path.send_keys("/home/travis/build/drewgallis/csec380-p1/flask/tests/video_test/luffy_test.jpg")
+    path = "/home/travis/build/drewgallis/csec380-p1/flask/tests/video_test/" + file_name
+    file_path.send_keys(path)
     uploadBTN = firefox.find_element_by_id('UploadFile')
     uploadBTN.click()
     if "Successfully Uploaded File:" in firefox.page_source:
         print("Success Caught: File Uploaded Succesfully " + video_name)
     return firefox
 
-def delete_video(firefox):
-    firefox.get('http://localhost:5000/uploads/delete/test123/LuffyBoi') # test against flask app
+def delete_video(firefox, video_name):
+    path = 'http://localhost:5000/uploads/delete/test123/' + video_name
+    firefox.get(path) # test against flask app
     print("Trying to delete image...")
-    print(firefox.page_source)
-    return
+    if video_name not in firefox.page_source:
+        print("Sucessfully Deleted File: " + video_name)
+    else:
+         print("Could NOT Deleted File: " + video_name)
+    return firefox
 
 def main():
     options = Options() # get firefox webdriver options
     options.add_argument('-headless') # run tests in headless mode CMD
     firefox = Firefox(firefox_options=options) # intialize firefox web driver
     firefox = getlogin(firefox)
-    firefox = upload_video(firefox, "LuffyBoi")
-    delete_video(firefox)
+    firefox = upload_video(firefox, "LuffyBoi", "luffy_test.jpg")
+    firefox = upload_video(firefox, "Rabbit", "rabbit_test.jpg")
+    firefox = delete_video(firefox, "LuffyBoi")
+    delete_video(firefox, "Rabbit")
     firefox.close()
 
 if __name__ == "__main__":
